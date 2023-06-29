1ffbd884b71f7eab8bb239ff60b0869640441bcc
Tassos Tavlarides <tavlarides@gmail.com>
2023-06-29 16:57:39 +0300

    Initial

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-from enum import StrEnum
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Form, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-ModelName = StrEnum('ModelName', ["alexnet", "resnet", "lenet"])
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+templates = Jinja2Templates(directory="templates")
 
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
+@app.get("/")
+async def hello(request: Request):
+    return templates.TemplateResponse(
+        "base.html", {"request": request, "title": "Jinja and FastAPI"}
+    )
 
-    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/form")
+async def form(request: Request):
+    return templates.TemplateResponse(
+        "form.html",
+        {
+            "request": request,
+            "name": "Lola",
+            "fields": [
+                {"name": "field1", "label": "Field 1", "type": "text"},
+                {"name": "field2", "label": "Field 2", "type": "text"},
+                {"name": "lola", "label": "Lola", "type": "text"},
+                # Add more fields as needed
+            ],
+        },
+    )
+
+
+@app.post("/submit")
+async def handleForm(name: str=Form(...), fields: list[str]=Form(...)):
+    # form_data = await request.form()
+    print(f"Name: {name}")
+
+    # for field in fields:
+    #     print(f"Field: {field}")
+
+    return {"message": "Data submitted successfully"}
